b8473277384b5caaf1cdac0427449108a82eaa13
ChernenkoDenys <denchernenko98@gmail.com>
2024-06-09 11:29:38 +0200

    Finish Permutation Generator

--- PermutationGenerator.py
+++ PermutationGenerator.py
@@ -1,15 +1,29 @@
 # Напишіть функцію-генератор перестановок, 
 # яка отримує список елементів 
 # і видає усі можливі перестановки цих елементів.
-from math import factorial
+
+
+def permutations(lst):
+    c = [0] * len(lst)
+    yield lst[:]
+    
+    i = 0
+    while i < len(lst):
+        if c[i] < i:
+            if i % 2 == 0:
+                lst[0], lst[i] = lst[i], lst[0]
+            else:
+                lst[c[i]], lst[i] = lst[i], lst[c[i]]
+            yield lst[:]
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+            i += 1
 
 
 
-def permutations(list_elements: list):
-    if len(list_elements) <= 1:
-            yield list_elements
+    
             
-        
-
 for i in permutations([1, 2, 3]):
- print(i)
+    print(i)
